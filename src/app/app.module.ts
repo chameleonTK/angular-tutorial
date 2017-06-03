2d5c415952ab1c55f01b94af15dd9a5bae278363
Pakawat Nakwijit <kramatk@gmail.com>
2017-06-03 23:42:22 +0700

    [service] use http instead of contant value

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule }   from '@angular/router';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { HeroService } from './hero.service';
 import { AppRoutingModule }     from './app-routing.module';
@@ -15,6 +19,8 @@ import { AppRoutingModule }     from './app-routing.module';
     imports: [ 
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         AppRoutingModule
     ],
     declarations: [ 
